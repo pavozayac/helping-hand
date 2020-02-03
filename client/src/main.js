fb5db43f05aacf044bb03e7cd3509fe3aba0d908
Pawel <exaus.lol@gmail.com>
2020-02-03 21:17:18 +0100

    Pierwszy commit

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import Drawer from './components/Drawer.vue'
 
 Vue.config.productionTip = false
+
+Vue.component('app-drawer', Drawer); 
 
 new Vue({
   vuetify,
