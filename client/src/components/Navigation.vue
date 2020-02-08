51b0229d994cc9251f6b5d932ed77de379ed67b2
pavozayac <exaus.lol@gmail.com>
2020-02-08 18:31:53 +0100

    Dopracowane menu

--- client/src/components/Navigation.vue
+++ client/src/components/Navigation.vue
@@ -3,14 +3,29 @@
 
     <v-app-bar app
       color="primary"
-      dark
-      :extended='extended'
+      dark 
+      :extended='onTop'
+      max-height='400'
+      :prominent='onTop'
+      v-scroll='checkScroll'
     >
-      <v-app-bar-nav-icon class="black--text" @click="drawer = !drawer">
+      <v-app-bar-nav-icon class="black--text" @click="drawer = !drawer" v-if='!onTop'>
       </v-app-bar-nav-icon>
-      <template v-slot:extension>
+
+      <v-toolbar-title class='font-weight-bold black--text montserrat' v-if='!onTop'>Helping Hand</v-toolbar-title>
+
+      <v-container fluid v-if='onTop'>
+        <v-row no-gutters>
+          <v-col class='d-inline-flex justify-space-around align-self-center'>
+            <v-img class='align-self-center' src='../assets/logo-dark.png' max-height='100' max-width='100' contain />
+          </v-col>
+        </v-row>
+      </v-container>
+
+      <template v-slot:extension v-if='onTop'>
         <v-container class='pr-10 pl-10' fluid>
           <v-row>
+            
             <v-col v-for='item in items' :key='item.title' class='d-inline-flex justify-space-around'>
               <v-btn block class='d-flex align-center black--text' depressed text link background="primary">{{ item.title }}</v-btn>
             </v-col>
@@ -53,14 +68,37 @@
 export default {
     data () {
         return { 
+            onTop: true,
             extended: true,
             drawer: false,
+            collapsed: false,
             items: [
-            { title: 'Dashboard', icon: 'announcement' },
-            { title: 'Account', icon: 'alarm' },
-            { title: 'Admin', icon: 'all_out' },
+            { title: 'Strona Główna', icon: 'house' },
+            { title: 'Wybrane', icon: 'stars' },
+            { title: 'Aplikuj', icon: 'person_add' },
+            { title: 'FAQ', icon: 'insert_comment'},
+            { title: 'Kontakt', icon: 'phone'}
             ],
        }
+    },
+    methods: {
+      checkScroll(){
+
+          if (window.scrollY == 0){
+            this.onTop = true
+          } else {
+            this.onTop = false
+          }
+
+      }
     }
 }
 </script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Montserrat:400,600&display=swap');
+
+.montserrat {
+    font-family: Montserrat, sans-serif;
+}
+</style>
